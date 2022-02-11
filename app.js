afd97bc15b9ac9fcfe05630e74fab56cd55cb752
riad960 <riad96048@gmail.com>
2022-02-11 21:15:08 +0600

    doone

--- app.js
+++ app.js
@@ -9,23 +9,58 @@ const score = document.querySelector(".score");
 const highscore = document.querySelector(".highscore");
 const display = document.querySelector(".number");
 //random number
-// let randomNumber = parseInt(Math.random() * 20);
-let randomNumber = 20;
+let randomNumber = parseInt(Math.random() * 21);
+let topScore = 0;
 let gameScore = 20;
 checkButton.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-
-  display.textContent = guess;
+  console.log(randomNumber);
   if (!guess) {
     message.textContent = "❌❌ Enter a value to play";
-  } else {
-    if (guess === randomNumber) {
-      document.body.style.background = "#4ade80";
-      message.textContent = "🎉🎉Correct Answer💥💥";
-    } else {
+  } else if (guess === randomNumber) {
+    display.textContent = guess;
+    display.style.width = "30rem";
+    document.body.style.background = "#4ade80";
+    message.textContent = "🎉🎉Correct Answer💥💥";
+    if (gameScore > topScore) {
+      topScore = gameScore;
+      highscore.textContent = topScore;
+    }
+  } else if (guess > randomNumber) {
+    if (gameScore > 1) {
       document.body.style.background = "#f50057";
-      message.textContent = "💢💢Wrong Guess";
-      score.textContent = gameScore--;
+      message.textContent = "📈Too high";
+      gameScore--;
+      score.textContent = gameScore;
+      setTimeout(function () {
+        document.body.style.background = "#222";
+      }, 1000);
+    } else {
+      message.textContent = "💥You lost the game";
+      score.textContent = 0;
+    }
+  } else if (guess < randomNumber) {
+    if (gameScore > 1) {
+      document.body.style.background = "#f50057";
+      message.textContent = "📉Too low";
+      gameScore--;
+      score.textContent = gameScore;
+      setTimeout(function () {
+        document.body.style.background = "#222";
+      }, 1000);
+    } else {
+      message.textContent = "💥You lost the game";
+      score.textContent = 0;
     }
   }
+});
+tryAgain.addEventListener("click", function () {
+  document.body.style.background = "#222";
+  gameScore = 20;
+  display.style.width = "15rem";
+  score.textContent = gameScore;
+  randomNumber = parseInt(Math.random() * 21);
+  document.querySelector(".guess").value = "";
+  message.textContent = "Start guessing...";
+  display.textContent = "?";
 });
